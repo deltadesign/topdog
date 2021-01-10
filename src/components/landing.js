71d4a86bccedf2289b4dc14fa070f43670d21316
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-01-10 18:45:26 +0000

    User login form created

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/row';
 
 
 class Landing extends React.Component{
@@ -17,13 +18,29 @@ class Landing extends React.Component{
 
     <div className = "landing">
       <Container className = "landingContainer">
-        <Form>
-          <Form.Group controlId = "userLoginName">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type = "email" placeholder = "Enter Your Email"></Form.Control>
-          </Form.Group>
-          <Button variant = "secondary" type="submit">Enter</Button>
+
+        <Row>
+          <h2 id="welcome-message">Welcome</h2>
+        </Row>
+        
+        
+        <Form className = "user-login-form">
+            <Form.Group controlId = "user-login-name">
+              <Form.Row className="align-items-center">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type = "email" placeholder = "Your Email Address"></Form.Control>
+              </Form.Row>
+            </Form.Group>
+            <Form.Group controlId = "user-password">
+              <Form.Row className="align-items-center">
+                <Form.Label>Password</Form.Label>       
+                <Form.Control type = "password" placeholder = "Password"></Form.Control>            
+              </Form.Row>
+            </Form.Group>
+              <Button variant = "secondary" type="submit">Submit</Button>
         </Form>
+
+
       </Container>
     </div>
     

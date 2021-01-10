c91594e5947a97d968c666c20b3b91353a738227
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-01-10 20:09:25 +0000

    Login from nowa component

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+//components
+import Login from './forms/loginform'
+
 // Bootstrap Elements
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/row';
 
 
@@ -23,23 +24,7 @@ class Landing extends React.Component{
           <h2 id="welcome-message">Welcome</h2>
         </Row>
         
-        
-        <Form className = "user-login-form">
-            <Form.Group controlId = "user-login-name">
-              <Form.Row className="align-items-center">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type = "email" placeholder = "Your Email Address"></Form.Control>
-              </Form.Row>
-            </Form.Group>
-            <Form.Group controlId = "user-password">
-              <Form.Row className="align-items-center">
-                <Form.Label>Password</Form.Label>       
-                <Form.Control type = "password" placeholder = "Password"></Form.Control>            
-              </Form.Row>
-            </Form.Group>
-              <Button variant = "secondary" type="submit">Submit</Button>
-        </Form>
-
+        <Login />
 
       </Container>
     </div>

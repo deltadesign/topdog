06db8c306fa2d3dfea8383d9e4d572cdfc8a8890
andi194 <andrea_dolan@hotmail.co.uk>
2021-01-11 18:07:18 +0000

    set up navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+// Bootstrap Elements
+// import Container from 'react-bootstrap/Container';
+
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+
+            <div className="navbar">
+            <div class="navbar">
+                
+                <a href="#about">About</a>
+                    <a href="#profile">Profile</a>
+                        <a href="#extra">Extra</a>
+                            <a href="#links">Links</a>
+                             <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                        </div>
+
+                    <div className="row">
+                        <h4>Top Dog Inc Logo</h4>
+                           </div>
+
+                  </div>     
+                   
+
+
+
+        )
+    }
+}
+
+export default Navbar;
+
+

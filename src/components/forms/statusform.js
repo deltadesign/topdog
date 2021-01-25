0aa52997887cc1bed6a5953ffabc0b8e697b7f83
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-01-25 14:14:46 +0000

    status form updated css

--- src/components/forms/statusform.js
+++ src/components/forms/statusform.js
@@ -30,11 +30,11 @@ export default class statusform extends React.Component {
   render() {
     return (
       <Form onSubmit = {(e) => this.submitHandler(e)} className='status-form'>  
-
+      
       <Form.Group controlId = "username">
         <Form.Control name = "username" type = "text" value = {this.state.username} placeholder="Username" onChange = {(e) => this.handleChange(e)} required />
       </Form.Group>
-
+     
       <Form.Group controlId = "status"> 
         <Form.Control name = "status" as = "textarea" rows={3} value = {this.state.status} placeholder="What's happening?" onChange = {(e) => this.handleChange(e)} required />
       </Form.Group>

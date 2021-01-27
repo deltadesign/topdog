56afef34bfa3138215488443a92fc94bb91f973c
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-01-27 15:21:52 +0000

    updated styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react'; // import react component
 import 'bootstrap/dist/css/bootstrap.min.css'; // add bootstrap styles
 import './App.css'; // add custom CSS values 
 
-// import './profile.css';
-
-
 // COMPONENTS
 import Landing from './components/landing';
 import Profile from './components/profile';
 import Feedpage from './components/Feedpage';
-
 
 // this app uses the class based system
 // this class contains the render method

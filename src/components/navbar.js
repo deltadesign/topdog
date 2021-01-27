927e1062c1a7e77496855b10c97e41f8e34a4cdd
unknown <alexkynoch@yahoo.co.uk>
2021-01-27 20:47:25 +0000

    icons added to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { GiDogHouse } from 'react-icons/gi';
 import { SiDatadog } from "react-icons/si";
+import { ImNewspaper } from "react-icons/im";
+import { GiExitDoor } from "react-icons/gi";
+
 
 import '../css/navbar.css'
 
@@ -16,12 +19,14 @@ class Navbar extends React.Component {
 
         <div className="navbar">
         
-                <h4 className="logo-text">Top Dog</h4>
-                
+                {/* <h4 className="logo-text">Top Dog</h4> */}
+                <div class="icon"><img src="https://img.icons8.com/pastel-glyph/64/000000/dog--v2.png"/></div>
+
                 <a href="#Home"><GiDogHouse style={{height:"40px", width:"40px"}}/></a>
                 <a href="#profile"><SiDatadog style={{height:"40px", width:"40px"}}/></a>
-                <a href="#Newsfeed">Newsfeed</a>
-                <a href="" onClick={()=>this.props.logout()}>Logout</a>
+                <a href="#Newsfeed"><ImNewspaper style={{height:"40px", width:"40px"}}/></a>
+                {/* <a href="#Newsfeed"><GiExitDoor style={{height:"40px", width:"40px"}}/></a> */}
+                <a href="" onClick={()=>this.props.logout()}><GiExitDoor style={{height:"40px", width:"40px"}}/></a>
                            
         </div>
         )

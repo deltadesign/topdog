df17a1c225997ca0cbdb4130fc527a63b64882ac
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-01-28 15:16:16 +0000

    css updates logo variable added

--- src/components/landing.js
+++ src/components/landing.js
@@ -26,7 +26,7 @@ class Landing extends React.Component{
     <div className = "landing-page">
       <div className="aboutarea">
         <div className="abouttext">
-          <h1>Top Dog</h1>
+          <h1 className="logo">Top Dog</h1>
           <hr/>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <hr/>

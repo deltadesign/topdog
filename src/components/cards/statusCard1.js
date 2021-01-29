2d6ab20d88b2b93ff88fc792efdc9642c90625eb
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-01-29 22:58:21 +0000

    solved comment card layout, added poops to status

--- src/components/cards/statusCard1.js
+++ src/components/cards/statusCard1.js
@@ -11,120 +11,147 @@ import Col from 'react-bootstrap/col';
 import Form from 'react-bootstrap/form';
 
 //REACT ICONS
-import {IoPawSharp} from 'react-icons/io5';
+import { IoPawSharp } from 'react-icons/io5';
+import { FaPoop } from 'react-icons/fa';
 
 //CUSTOM CSS
 import '../../css/statuscard.css'
 
-class StatusCard1 extends React.Component{
-  constructor(props){
-  super(props)
-  this.state = {
-    likes: 0,
-    liked: false,
-    comment:'',
-    comments: [] 
+class StatusCard1 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      poops: 0,
+      pooped: false,
+      likes: 0,
+      liked: false,
+      comment: '',
+      comments: []
     }
   }
 
   //HANDLES COMMENT TEXT
-  handleChange(e){
+  handleChange(e) {
     this.setState(() => ({
       comment: e.target.value
     }))
   }
 
   //HANDLES ADDING COMMENTS
-  addComment(e){
+  addComment(e) {
     // console.log(e.target)
     const comment = this.state.comment;
     const comments = this.state.comments;
 
-    if(comment.length === 0){
+    if (comment.length === 0) {
       alert('doin a bamboozle.. comments cannot be blank')
-    }else{
-    this.setState(() => ({
-      comments: [...comments, comment],
-      comment: ""
-    }))
+    } else {
+      this.setState(() => ({
+        comments: [...comments, comment],
+        comment: ""
+      }))
     }
     e.preventDefault();
   }
 
   //RENDERS ALL COMMENTS
-  renderComments(){
+  renderComments() {
     return this.state.comments.map((comment, i) => (
-      <CommentCard key={i} comment = {comment} />
+      <CommentCard key={i} comment={comment} />
     ))
   }
 
   //ADDS & REMOVES A LIKE
-  pawsome(){
+  pawsome() {
     const liked = this.state.liked;
 
-    if(liked === false){
+    if (liked === false) {
       this.setState(() => ({
-        likes: this.state.likes+1,
+        likes: this.state.likes + 1,
         liked: true
       }))
     } else {
       this.setState(() => ({
-        likes: this.state.likes-1,
+        likes: this.state.likes - 1,
         liked: false
       }))
     }
   }
 
-  render(){
+    //ADDS & REMOVES A POOP
+    poopsome() {
+      const pooped = this.state.pooped;
+  
+      if (pooped === false) {
+        this.setState(() => ({
+          poops: this.state.poops + 1,
+          pooped: true
+        }))
+      } else {
+        this.setState(() => ({
+          poops: this.state.poops - 1,
+          pooped: false
+        }))
+      }
+    }
+
+  render() {
     return (
       <div className="status-card-wrapper">
-        
+
         <div class="status-card-content">
 
           <div className="status-card-colleft">
-            <div id="userImage1"></div> 
+            <div id="userImage1"></div>
           </div>
 
           <div className="status-text">
             <div className="status-card-colmid">
-              <h3 className = "username-text">Bob's status update</h3>
+              <h3 className="username-text">Bob's status update</h3>
               <h5 className="post-content">{this.props.text}</h5>
             </div>
 
             <div className="reaction-row">
+
               <div className="buttonbox">
-                <div onClick={()=>this.pawsome()}><IoPawSharp/></div>
+                <div onClick={() => this.poopsome()} style={this.state.pooped ? { color: "brown" } : { color: "black" }} ><FaPoop /></div>
+                <div className="likes" >{this.state.poops}</div>
+              </div>
+
+              <div className="buttonbox">
+                <div onClick={() => this.pawsome()} style={this.state.liked ? { color: "green" } : { color: "black" }}><IoPawSharp /></div>
                 <div className="likes">{this.state.likes}</div>
-             </div>
-          </div>
+              </div>
+
+            </div>
 
           </div>
 
-      </div>
+        </div>
 
         <Row className="comments-row">
           <Col>{this.renderComments()}</Col>
         </Row>
-       
-          <Form onSubmit = {(e) => this.addComment(e)}>
-            <Form.Group className="m-3" controlId="comment">
-              <Form.Row>
-                
-                  <Form.Control
-                  placeholder="Add a Comment..."
-                  name = "comment"
-                  onChange = {(e) => this.handleChange(e)}
-                  value = {this.state.comment}
-                  />
-                  <div className="submit-btn-wrapper">
-                    <Button variant="outline-dark" className="submit-btn" type="submit">Comment</Button>
-                  </div>
-              </Form.Row>
-            </Form.Group>
-          </Form>
-        
+
+        <Form onSubmit={(e) => this.addComment(e)}>
+          <Form.Group className="m-3" controlId="comment">
+            <Form.Row>
+
+              <Form.Control
+                placeholder="Add a Comment..."
+                name="comment"
+                onChange={(e) => this.handleChange(e)}
+                value={this.state.comment}
+              />
+              <div className="submit-btn-wrapper">
+                <Button variant="outline-dark" className="submit-btn" type="submit">Comment</Button>
+              </div>
+            </Form.Row>
+          </Form.Group>
+        </Form>
+
       </div>
-     );
+    );
   }
 }
 

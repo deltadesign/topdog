a9ded47d09956c0d95dc176bbb08311a35210365
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-01-29 17:24:57 +0000

    updated a lot css and layouts

--- src/components/cards/viewprofileCard.js
+++ src/components/cards/viewprofileCard.js
@@ -1,44 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Card from 'react-bootstrap/card'
-import Button from 'react-bootstrap/button';
-import Row from 'react-bootstrap/row';
-import Col from 'react-bootstrap/col';
 
 // CUSTOM CSS
 import '../../css/viewprofilecard.css';
 
-
-
-class ViewprofileCard extends React.Component {
+export default class ViewprofileCard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {}
     }
 
-
     render() {
         return (
             <>
-                <div className="profile-card">
-                    <div className="upper-container">
-                        <div className="image-container">
-                           
-                        </div>
-                    </div>
-                     <h3>Dogs who viewed profile</h3>
-                
-                     <div id="userImage2"></div><h5>Doug</h5>
-                     <div id="userImage3"></div><h5>Ted</h5>
-                     <div id="userImage4"></div><h5>Pete</h5>
-                     <div id="userImage5"></div><h5>Kat</h5>
-                   
+                <div className="view-profile-image-container">
+                    <h4 id="view-profile-text">Recent Views</h4>
+                    <br />
+                    <div id="userImage2"></div><h5>Doug</h5>
+                    <div id="userImage3"></div><h5>Ted</h5>
+                    <div id="userImage4"></div><h5>Pete</h5>
+                    <div id="userImage5"></div><h5>Kat</h5>
                 </div>
             </>
         );
     }
 }
 
-
-
-export default ViewprofileCard;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,11 +22,11 @@ class Navbar extends React.Component {
                 {/* <h4 className="logo-text">Top Dog</h4> */}
                 <div class="icon"><img src="https://img.icons8.com/pastel-glyph/64/000000/dog--v2.png"/></div>
 
-                <a href="#Home"><GiDogHouse style={{height:"40px", width:"40px"}}/></a>
-                <a href="#profile"><SiDatadog style={{height:"40px", width:"40px"}}/></a>
-                <a href="#Newsfeed"><ImNewspaper style={{height:"40px", width:"40px"}}/></a>
+                <a href="#Home" title="home"><GiDogHouse className="navbar-icon"/></a>
+                <a href="#profile"><SiDatadog className="navbar-icon"/></a>
+                <a href="#Newsfeed"><ImNewspaper className="navbar-icon"/></a>
                 {/* <a href="#Newsfeed"><GiExitDoor style={{height:"40px", width:"40px"}}/></a> */}
-                <a href="" onClick={()=>this.props.logout()}><GiExitDoor style={{height:"40px", width:"40px"}}/></a>
+                <a href="" onClick={()=>this.props.logout()}><GiExitDoor className="navbar-icon"/></a>
                            
         </div>
         )

88a3af5aa0cbdefaf44ccee296b1aebfee5f0064
andi194 <andrea_dolan@hotmail.co.uk>
2021-02-01 16:00:54 +0000

    newgroupscard

--- src/components/cards/suggestedGroupsCard.js
+++ src/components/cards/suggestedGroupsCard.js
@@ -10,12 +10,20 @@ export default class SuggestedGroupscards extends React.Component {
             <>
                 <div className="suggested-groups-container">
                     <h4 id="view-profile-text">Suggested Groups</h4>
-                    <br />
-                    <div id="userImage6"></div><h5>Dog Poker Group</h5>
+                     <br />
+                     <div className="sugg-groups-left">
+                    <div id="userImage6"><div className="spot-on1"></div></div><h5>Dog Poker Group</h5>
+                     <h6>1 new post</h6>
+                    </div>
                     <div id="userImage7"></div><h5>Dog Sledding Group</h5>
+                    <h6>Updated 5 hours ago</h6>
                     <div id="userImage8"></div><h5>Dog Film Group</h5>
-                    <div id="userImage9"></div><h5>Dog Walking Group</h5>
-                </div>
+                    <h6>Updated 12 hours ago</h6>
+                    <div id="userImage9"><div className="spot-on1"></div></div><h5>Dog Walking Group</h5>
+                    <h6>2 new posts</h6>
+                    </div>
+                    
+                
             </>
         );
     }

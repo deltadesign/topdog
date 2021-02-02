93c7cdb9f7449ac468289a087cd78f819592fc1b
unknown <alexkynoch@yahoo.co.uk>
2021-02-02 16:57:20 +0000

    camera icon added

--- src/components/cards/feedCard.js
+++ src/components/cards/feedCard.js
@@ -2,8 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { TiCamera } from "react-icons/ti";
 // CUSTOM CSS
+
 import '../../css/feedcard.css'
-import { Link } from 'react-router-dom';
+
 class Feed extends React.Component {
     constructor(props) {
         super(props)
@@ -14,18 +15,13 @@ class Feed extends React.Component {
         return (
             <>
                 <div className="profile-header-card">
-                    
-                        <div className="image-container">
-                            <div id="userImage">
-                            <div className="feedcard-icon-wrapper">
-                        
-                        <Link to="/topdog" title="upload image"><TiCamera className="feedcard-icon"/></Link>
-                        
-                    </div>
-                            <div className="spot-on2"></div>
-                            </div>
+
+                    <div className="image-container">
+                        <div id="userImage"> 
+                                <div title="upload image"><TiCamera className="feedcard-icon" /></div>
                         </div>
-                    
+                    </div>
+
 
                     <h3>{this.props.username}</h3>
                     <h4>Head of Security</h4>
